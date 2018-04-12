e27ee72b5637f8df9cc30b37ee828d8a1f597e14
Roberto Machorro <RobertoMachorro@users.noreply.github.com>
2018-04-11 22:02:06 -0400

    Added swipe gestures programmatically, in all directions, logs action.

--- SwipeSample/ViewController.m
+++ SwipeSample/ViewController.m
@@ -16,7 +16,11 @@
 
 - (void)viewDidLoad {
 	[super viewDidLoad];
-	// Do any additional setup after loading the view, typically from a nib.
+
+	[self addSwipeWithDirection:UISwipeGestureRecognizerDirectionLeft action:@selector(swipeLeft:)];
+	[self addSwipeWithDirection:UISwipeGestureRecognizerDirectionRight action:@selector(swipeRight:)];
+	[self addSwipeWithDirection:UISwipeGestureRecognizerDirectionUp action:@selector(swipeUp:)];
+	[self addSwipeWithDirection:UISwipeGestureRecognizerDirectionDown action:@selector(swipeDown:)];
 }
 
 
@@ -25,5 +29,28 @@
 	// Dispose of any resources that can be recreated.
 }
 
+#pragma mark - Swipe Gestures
+
+- (void)addSwipeWithDirection:(UISwipeGestureRecognizerDirection)direction action:(SEL)method {
+	UISwipeGestureRecognizer *gestureRecognizer = [[UISwipeGestureRecognizer alloc] initWithTarget:self action:method];
+	[gestureRecognizer setDirection:direction];
+	[self.view addGestureRecognizer:gestureRecognizer];
+}
+
+- (IBAction)swipeLeft:(UISwipeGestureRecognizer*)sender {
+	NSLog(@"Swipe Left: %@", sender);
+}
+
+- (IBAction)swipeRight:(UISwipeGestureRecognizer*)sender {
+	NSLog(@"Swipe Right: %@", sender);
+}
+
+- (IBAction)swipeUp:(UISwipeGestureRecognizer*)sender {
+	NSLog(@"Swipe Up: %@", sender);
+}
+
+- (IBAction)swipeDown:(UISwipeGestureRecognizer*)sender {
+	NSLog(@"Swipe Down: %@", sender);
+}
 
 @end
